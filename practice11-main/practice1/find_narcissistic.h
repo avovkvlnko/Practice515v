156b7487da38d9353e71e3b8f0efe6d110e2b057
avovkvlnko <112502722+avovkvlnko@users.noreply.github.com>
2023-02-24 15:36:42 +0200

    Update find_narcissistic.h

--- practice11-main/practice1/find_narcissistic.h
+++ practice11-main/practice1/find_narcissistic.h
@@ -1,3 +1,12 @@
+/**
+* @file find_narcissistic.h
+* @author Коваленко В.А, гр. 515-В
+* @date 20 серпня     2022
+* @brief Навчальна практика.
+*
+* 34 Варіант - Числа Армстронга
+*/
+
 #ifndef FIND_NARCISSISTIC_H
 #define FIND_NARCISSISTIC_H
 
